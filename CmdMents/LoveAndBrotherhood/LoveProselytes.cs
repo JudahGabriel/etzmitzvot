14e071f9adecd0108ab004764b7093c98ecaffdb
tuggyne_cp <SND\tuggyne_cp>
2009-10-10 22:36:04 -0700

    + Added two new commandments to make up for last week's miss: LoveJews and LoveProselytes. = Added author information to the commandments I've entered.

---
+++ CmdMents/LoveAndBrotherhood/LoveProselytes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CmdMents.LoveAndBrotherhood
+{
+    /// <author>NET</author>
+    class LoveProselytes : LoveNeighborAsSelf
+    {
+        public LoveProselytes()
+        {
+            base.Book = CommandmentBook.Deuteronomy;
+            base.CanBeCarriedOutToday = true;
+            base.Chapter = 10;
+            base.CommandmentType = CommandmentType.Positive;
+
+            // Hmmm... this is dicey. I really don't know that I can say off the top of my head.
+            // I think, though, that this is likely to be correct.
+            base.FollowedByChristians = true;
+            base.FollowedByMessianics = true;
+            base.FollowedByObservantJews = true;
+
+            base.Number = 14;
+            base.ShortSummary = "Love converts.";
+            base.Text = "And you are to love those who are aliens, for you yourselves were aliens in Egypt.";
+            base.Verse = 19;
+        }
+    }
+}
--- CmdMents/Torah/ListenToProphet.cs
+++ CmdMents/Torah/ListenToProphet.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace CmdMents.Torah
 {
+    /// <author>NET</author>
     class ListenToProphet : KeepGodsCommandments
     {
         public ListenToProphet()
