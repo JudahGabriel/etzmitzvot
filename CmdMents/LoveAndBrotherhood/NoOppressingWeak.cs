db835e1c88cceff87578255e5c1b01a35f1af489
tuggyne_cp <SND\tuggyne_cp>
2009-10-29 00:27:21 -0700

    + Added No Oppressing Weak, No Slandering, and No Taking Revenge commandments, all of which derive indirectly from LoveNeighborAsSelf.

---
+++ CmdMents/LoveAndBrotherhood/NoOppressingWeak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CmdMents.LoveAndBrotherhood
+{
+    /// <author>NET</author>
+    /// <remarks>Derivation as conventionally stated should be LoveNeighborAsSelf; 
+    /// the text, however, suggests deriving from LoveSojourners.</remarks>
+    class NoOppressingWeak : LoveSojourners
+    {
+        public NoOppressingWeak()
+        {
+            base.Book = CommandmentBook.Exodus;
+            base.CanBeCarriedOutToday = true;
+            base.Chapter = 22;
+            base.CommandmentType = CommandmentType.Negative;
+
+            // In this particular commandment, I assume that "followed by X" means 
+            //      "followed by X when convenient, and generally agreed to loudly 
+            //      by every member of X at all times".
+            base.FollowedByChristians = true;
+            base.FollowedByMessianics = true;
+            base.FollowedByObservantJews = true;
+
+            base.Number = 18;
+            base.ShortSummary = "No oppressing or mistreating the weak.";
+            base.Text = @"""Do not mistreat an alien or oppress him, for you were aliens in Egypt.""";
+            base.Verse = 21;
+        }
+    }
+}
---
+++ CmdMents/LoveAndBrotherhood/NoSlandering.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CmdMents.LoveAndBrotherhood
+{
+    class NoSlandering : NoHatingYourBrother
+    {
+        public NoSlandering()
+        {
+            base.Book = CommandmentBook.Leviticus;
+            base.CanBeCarriedOutToday = true;
+            base.Chapter = 19;
+            base.CommandmentType = CommandmentType.Negative;
+
+            // Once again, we have a case of hypocrisy between acknowledgement 
+            // and action. All groups acknowledge this commandment, but many 
+            // are bad at following this. Still, on the whole, I consider that 
+            // the majority of orthodox believers all follow the command to a 
+            // considerable degree.
+            base.FollowedByChristians = true;
+            base.FollowedByMessianics = true;
+            base.FollowedByObservantJews = true;
+
+            base.Number = 19;
+            base.ShortSummary = "No slandering your brother.";
+            base.Text = @""" 'Do not go about spreading slander among your people." + "\n" +
+                        @""" 'Do not do anything that endangers your neighbor's life. I am the LORD.";
+            base.Verse = 16;
+        }
+
+    }
+}
