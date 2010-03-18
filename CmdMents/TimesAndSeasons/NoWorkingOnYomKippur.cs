0411e609cd7fb945a0388bd1266529556212ba67
JudahGabriel_cp <SND\JudahGabriel_cp>
2010-03-18 11:03:31 -0700

    Added new year commandment.

--- CmdMents/TimesAndSeasons/NoWorkingOnYomKippur.cs
+++ CmdMents/TimesAndSeasons/NoWorkingOnYomKippur.cs
@@ -6,7 +6,7 @@ using CmdMents.Torah;
 
 namespace CmdMents.TimesAndSeasons
 {
-    class NoWorkingOnYomKippur : KeepGodsCommandments
+    class NoWorkingOnYomKippur : ObserveNewYearInFirstMonth
     {
         public NoWorkingOnYomKippur()
         {
@@ -18,7 +18,7 @@ namespace CmdMents.TimesAndSeasons
             base.FollowedByMessianics = CommandmentObedience.Obeyed;
             base.FollowedByObservantJews = CommandmentObedience.Obeyed;
             base.Number = 93;
-            base.ShortSummary = "Do not work on Yom Kippur.";
+            base.ShortSummary = "No working on Yom Kippur.";
             base.Text = "You shall do no work at all. This is to be a lasting ordinance for the generations to come, wherever you live.";
             base.Verse = 31; // 32 in Jewish Bibles.
         }
